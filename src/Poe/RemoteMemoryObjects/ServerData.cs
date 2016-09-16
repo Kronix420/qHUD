f6d3b5f5b325451f3e38010c2ccebc34ad532acd
vmv <mcnvictor@gmail.com>
2016-09-16 09:29:31 +0300

    update offsets

--- src/Poe/RemoteMemoryObjects/ServerData.cs
+++ src/Poe/RemoteMemoryObjects/ServerData.cs
@@ -2,10 +2,6 @@ namespace qHUD.Poe.RemoteMemoryObjects
 {
     public class ServerData : RemoteMemoryObject
     {
-<<<<<<< HEAD
         public bool IsInGame => M.ReadInt(Address + 0x3934) == 3;
-=======
-        public bool IsInGame => M.ReadInt(Address + 0x38EC) == 3;
->>>>>>> origin/master
     }
 }
